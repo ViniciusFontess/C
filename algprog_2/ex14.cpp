e0dc155e6811c9c493a3e1c08e1521ab3b8e10f1
Vinicius <163762205+Vinizin7788@users.noreply.github.com>
2024-08-14 07:20:16 -0400

    Update ex14.cpp

--- algprog_2/ex14.cpp
+++ algprog_2/ex14.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
+#include <cstring>
+
 int main (){
-    char nome[], letras[],letras1[],final[];
-    int i;
+    char nome[10], letras[10],letras1[10],final[10];
+    int i, tamanho;
     scanf("%s", nome);
         letras = strcat(nome[0], nome[1]);
         letras1 = strcat(nome[2], nome[3]);
         final = strcat(letras,letras1);
-    printf(final);
-
+    tamanho = strlen(final);     
+        for (i = 0; i< tamanho; i++){
+             printf(final[i]"\n");
+        }
+  
     return 0;
 }
